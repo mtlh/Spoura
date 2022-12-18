9095d43124ad82541db3356dd7f6f20be08b971b
Matthew Harvey <matthewtlharvey@gmail.com>
2022-12-18 03:17:21 +0000

    img class for product

--- src/routes/product/[productid]/+page.svelte
+++ src/routes/product/[productid]/+page.svelte
@@ -6,11 +6,11 @@
 </script>
 
 {#if product}
-    <div class="p-10 text-center text-3xl">
+    <div class="p-10 text-center text-3xl m-auto">
         <h1>{product.id}</h1>
         <h1>{product.name}</h1>
         <h1>{product.price}</h1>
-        <img src={"/" + product.imgurl.main} alt={product.name}>
+        <img src={"/" + product.imgurl.main} alt={product.name} class="w-60 m-auto">
     </div>
 {:else}
 	<p>Not a valid product id.</p>
