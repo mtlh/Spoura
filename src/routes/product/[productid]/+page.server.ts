dc243cb0c0326771b97fe9ef3c12f41a909c9e0c
Matthew Harvey <matthewtlharvey@gmail.com>
2022-12-18 03:13:05 +0000

    schema price update + product function

---
+++ src/routes/product/[productid]/+page.server.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */ 
+import { PrismaClient } from '@prisma/client';
+ 
+const prisma = new PrismaClient();
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }: any) {
+
+    const product = await prisma.product.findFirst({
+        where: {
+            id: parseInt(params.productid)
+        }
+    })
+    
+    if (product) {
+        return {
+            product: {
+                "id": product?.id,
+                "name": product?.name,
+                "price": product?.price.toString(),
+                "imgurl": product?.imgurl,
+                "colour": product?.colour,
+                "fit": product?.fit,
+                "size": product?.size,
+                "review": product?.review,
+            }
+    
+        };
+    } else {
+        return {
+            "error": true
+        }
+    }
+}
---
+++ src/routes/product/[productid]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    /** @type {import('./$types').PageData} */
+    export let data;
+    const product = data.product;
+    console.log(product);
+</script>
+
+{#if product}
+    <div class="p-10 text-center text-3xl">
+        <h1>{product.id}</h1>
+        <h1>{product.name}</h1>
+        <h1>{product.price}</h1>
+        <img src={"/" + product.imgurl.main} alt={product.name}>
+    </div>
+{:else}
+	<p>Not a valid product id.</p>
+{/if}
