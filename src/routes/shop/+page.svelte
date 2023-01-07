ed20c543abf23adfa20ce08e2d7a5edd9141741a
Matthew Harvey <matthewtlharvey@gmail.com>
2023-01-07 03:14:33 +0000

    autocomplete filters

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -2,67 +2,32 @@
     import type { PageData } from '../products/$types';
     export let data : PageData;
     const all = data.all;
+    // @ts-ignore
+    import AutoComplete from "simple-svelte-autocomplete";
+    let sort_arr = ["All", "Latest", "Trending"] as const;
+    let sort_select: string;
+    let brand_arr = ["Nike", "Adidas", "Puma", "Under Armour"] as const;
+    let brand_select: string;
+    let type_arr = ["Nike", "Adidas", "Puma", "Under Armour"] as const;
+    let type_select: string;
 </script>
 
 {#if all}
     <div class="grid">
-        <div class="text-center lg:grid lg:fixed justify-center m-auto items-stretch max-w-7xl min-w-full bg-white sticky">
-            <p>Shop Filters</p>
-            <div class="grid grid-cols-3">
-                <div id="type">
-                    <p>TYPE</p>
-                    <div class="form-control w-60">
-                        <label class="cursor-pointer label">
-                        <span class="label-text pr-2">Mens</span>
-                        <input type="checkbox" class="checkbox checkbox-info" />
-                        </label>
-                        <label class="cursor-pointer label">
-                            <span class="label-text pr-2">Womens</span>
-                            <input type="checkbox" class="checkbox checkbox-info" />
-                        </label>
-                        <label class="cursor-pointer label">
-                            <span class="label-text pr-2">Kids</span>
-                            <input type="checkbox" class="checkbox checkbox-info" />
-                        </label>
-                    </div>
+        <div class="lg:grid lg:fixed justify-center m-auto items-stretch max-w-7xl min-w-full bg-white sticky">
+            <div class="grid grid-cols-3 p-2">
+                <div id="type" class="p-2">
+                    <AutoComplete items="{type_arr}" bind:selectedItem="{type_select}" placeholder="Type" class="rounded-lg h-auto p-2 bg-slate-200 w-72" />
                 </div>
-                <div id="sort">
-                    <p>SORT</p>
-                    <div class="form-control w-60">
-                        <label class="cursor-pointer label">
-                        <span class="label-text pr-2">Mens</span>
-                        <input type="checkbox" class="checkbox checkbox-info" />
-                        </label>
-                        <label class="cursor-pointer label">
-                            <span class="label-text pr-2">Womens</span>
-                            <input type="checkbox" class="checkbox checkbox-info" />
-                        </label>
-                        <label class="cursor-pointer label">
-                            <span class="label-text pr-2">Kids</span>
-                            <input type="checkbox" class="checkbox checkbox-info" />
-                        </label>
-                    </div>
+                <div id="sort" class="p-2">
+                    <AutoComplete items="{sort_arr}" bind:selectedItem="{sort_select}" placeholder="Sort" class="rounded-lg h-auto p-2 bg-slate-200 w-72" />
                 </div>
-                <div id="brand">
-                    <p>BRAND</p>
-                    <div class="form-control w-60">
-                        <label class="cursor-pointer label">
-                        <span class="label-text pr-2">Mens</span>
-                        <input type="checkbox" class="checkbox checkbox-info" />
-                        </label>
-                        <label class="cursor-pointer label">
-                            <span class="label-text pr-2">Womens</span>
-                            <input type="checkbox" class="checkbox checkbox-info" />
-                        </label>
-                        <label class="cursor-pointer label">
-                            <span class="label-text pr-2">Kids</span>
-                            <input type="checkbox" class="checkbox checkbox-info" />
-                        </label>
-                    </div>
+                <div id="brand" class="p-2">
+                    <AutoComplete items="{brand_arr}" bind:selectedItem="{brand_select}" placeholder="Brand" class="rounded-lg h-auto p-2 bg-slate-200 w-72" />
                 </div>
             </div>
         </div>
-        <div class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-center m-auto items-stretch mt-40 max-w-7xl">
+        <div class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-center m-auto items-stretch mt-10 max-w-7xl">
             {#each JSON.parse(all) as product}
                     <div class="my-6">
                         <a href="/product/{product.id}" class="m-auto">
