e163ea3928532559a419d70248cfb2c32a34b9b8
Matthew Harvey <matthewtlharvey@gmail.com>
2023-01-07 02:20:13 +0000

    sticky shop header

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -5,27 +5,65 @@
 </script>
 
 {#if all}
-    <div class="grid grid-cols-1 lg:grid-cols-6 justify-center md:p-14 max-w-7xl items-stretch gap-4">
-        <div class="col-span-2">
+    <div class="grid">
+        <div class="text-center lg:grid lg:fixed justify-center m-auto items-stretch max-w-7xl min-w-full bg-white sticky">
             <p>Shop Filters</p>
-            <div class="form-control w-60">
-                <label class="cursor-pointer label">
-                  <span class="label-text pr-2">Mens</span>
-                  <input type="checkbox" class="checkbox checkbox-info" />
-                </label>
-                <label class="cursor-pointer label">
-                    <span class="label-text pr-2">Womens</span>
-                    <input type="checkbox" class="checkbox checkbox-info" />
-                  </label>
-                  <label class="cursor-pointer label">
-                    <span class="label-text pr-2">Kids</span>
-                    <input type="checkbox" class="checkbox checkbox-info" />
-                  </label>
+            <div class="grid grid-cols-3">
+                <div id="type">
+                    <p>TYPE</p>
+                    <div class="form-control w-60">
+                        <label class="cursor-pointer label">
+                        <span class="label-text pr-2">Mens</span>
+                        <input type="checkbox" class="checkbox checkbox-info" />
+                        </label>
+                        <label class="cursor-pointer label">
+                            <span class="label-text pr-2">Womens</span>
+                            <input type="checkbox" class="checkbox checkbox-info" />
+                        </label>
+                        <label class="cursor-pointer label">
+                            <span class="label-text pr-2">Kids</span>
+                            <input type="checkbox" class="checkbox checkbox-info" />
+                        </label>
+                    </div>
+                </div>
+                <div id="sort">
+                    <p>SORT</p>
+                    <div class="form-control w-60">
+                        <label class="cursor-pointer label">
+                        <span class="label-text pr-2">Mens</span>
+                        <input type="checkbox" class="checkbox checkbox-info" />
+                        </label>
+                        <label class="cursor-pointer label">
+                            <span class="label-text pr-2">Womens</span>
+                            <input type="checkbox" class="checkbox checkbox-info" />
+                        </label>
+                        <label class="cursor-pointer label">
+                            <span class="label-text pr-2">Kids</span>
+                            <input type="checkbox" class="checkbox checkbox-info" />
+                        </label>
+                    </div>
+                </div>
+                <div id="brand">
+                    <p>BRAND</p>
+                    <div class="form-control w-60">
+                        <label class="cursor-pointer label">
+                        <span class="label-text pr-2">Mens</span>
+                        <input type="checkbox" class="checkbox checkbox-info" />
+                        </label>
+                        <label class="cursor-pointer label">
+                            <span class="label-text pr-2">Womens</span>
+                            <input type="checkbox" class="checkbox checkbox-info" />
+                        </label>
+                        <label class="cursor-pointer label">
+                            <span class="label-text pr-2">Kids</span>
+                            <input type="checkbox" class="checkbox checkbox-info" />
+                        </label>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-span-4">
-            <div class="grid grid-cols-1 md:grid-cols-3 justify-center m-auto items-stretch">
-                {#each JSON.parse(all) as product}
+        <div class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-center m-auto items-stretch mt-40 max-w-7xl">
+            {#each JSON.parse(all) as product}
                     <div class="my-6">
                         <a href="/product/{product.id}" class="m-auto">
                             <div class="card h-60 w-72 bg-center bg-cover -z-10" style='background-image: url({product.imgurl.main});'>
@@ -37,8 +75,7 @@
                             </div>
                         </a>
                     </div>
-                {/each}
-            </div>
+            {/each}
         </div>
     </div>
 {:else}
